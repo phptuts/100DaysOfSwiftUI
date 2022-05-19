4bdfe8f3e1e87f942b586827207e30f40818aee9
phptuts <glaserpower@gmail.com>
2022-05-18 22:14:34 -0700

    Day 30

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Noah Glaser on 5/17/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
