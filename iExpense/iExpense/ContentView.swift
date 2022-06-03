45dd8102900f4be2b42196d40a379e93c05cc808
phptuts <glaserpower@gmail.com>
2022-06-02 22:47:53 -0700

    Day 38 Challenges

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -8,7 +8,47 @@
 import SwiftUI
 
 
-
+struct ExpenseItemView: View {
+    
+    let item: ExpenseItem
+    
+    var textColor: Color {
+        if item.amount < 10 {
+            return .green
+        } else if item.amount < 100 {
+            return .purple
+        } else {
+            return .red
+        }
+    }
+    
+    var fontMoney: Font {
+        if item.amount < 10 {
+            return .body
+        } else if item.amount < 100 {
+            return .title
+        } else {
+            return .largeTitle
+        }
+    }
+    
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                
+                Text(item.name)
+                    .font(.headline)
+                    .foregroundColor(textColor)
+                Text(item.type)
+                    .foregroundColor(textColor)
+            }
+            Spacer()
+            Text(item.amount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                .font(fontMoney)
+                .foregroundColor(textColor)
+        }
+    }
+}
 
 struct ContentView: View {
     
@@ -18,18 +58,18 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             List {
-                ForEach(expenses.items) { item in
-                    HStack {
-                        VStack(alignment: .leading) {
-                            Text(item.name)
-                                .font(.headline)
-                            Text(item.type)
-                        }
-                        Spacer()
-                        Text(item.amount, format: .currency(code: "USD"))
-                    }
-                   
-                }.onDelete(perform: removeItems)
+                Section("Business") {
+                    ForEach(expenses.items.filter{ $0.type == "Business"}) { item in
+                        ExpenseItemView(item: item)
+                    }.onDelete(perform: removeItems(type: "Business"))
+                }
+                Section("Personal") {
+                    ForEach(expenses.items.filter{ $0.type == "Personal"}) { item in
+                        ExpenseItemView(item: item)
+                    }.onDelete(perform: removeItems(type: "Personal"))
+
+                }
+                
             }.navigationTitle("IExpense")
                 .toolbar {
                     Button {
@@ -44,8 +84,15 @@ struct ContentView: View {
         }
     }
     
-    func removeItems(at offsets: IndexSet) {
-        expenses.items.remove(atOffsets: offsets)
+    func removeItems(type: String) -> (IndexSet) -> Void {
+        return { offsets in
+           var itemTypeFiltered = expenses.items.filter { $0.type == type }
+            itemTypeFiltered.remove(atOffsets: offsets)
+            let nonEditItems = expenses.items.filter { $0.type != type }
+            expenses.items = expenses.items.filter { item in
+                itemTypeFiltered.contains(where: {$0.id == item.id}) || nonEditItems.contains(where: {$0.id == item.id})
+            }
+        }
     }
     
 }
