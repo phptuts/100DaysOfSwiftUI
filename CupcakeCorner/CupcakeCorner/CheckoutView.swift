cbc0a33329c6431af9408018cf8530c080e5efbf
phptuts <glaserpower@gmail.com>
2022-07-05 22:32:05 -0700

    Day 51

--- CupcakeCorner/CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -11,6 +11,9 @@ struct CheckoutView: View {
     
     @ObservedObject var order: Order
     
+    @State private var confirmationMessage = ""
+    @State private var showConfirmation = false
+    
     var body: some View {
         ScrollView {
             VStack {
@@ -25,12 +28,44 @@ struct CheckoutView: View {
                 Text("Your total is \(order.cost, format: .currency(code: "USD"))")
                     .font(.title)
                 Button("Place Order") {
-                    
+                    Task {
+                       await placeOrder()
+                    }
                 }.padding()
             }
         }.navigationTitle("Checkout")
             .navigationBarTitleDisplayMode(.inline)
+            .alert("Tahnk you!", isPresented: $showConfirmation) {
+                Button("OK") {
+                    
+                }
+            } message: {
+                Text(confirmationMessage)
+            }
         
+        
+        
+    }
+    func placeOrder() async {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Falied to encode order")
+            return
+        }
+        
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+            
+            let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+            confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on the way."
+            showConfirmation = true
+        } catch {
+            print("Checkout failed")
+        }
     }
 }
 
