05dddac4bb88178958809c94b067bd7faded257a
phptuts <glaserpower@gmail.com>
2022-08-03 08:53:27 -0700

    Day 62

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Noah Glaser on 8/3/22.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
