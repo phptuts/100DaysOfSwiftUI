8fccaa64750b416fe5f428e971a3229344395a6a
phptuts <glaserpower@gmail.com>
2022-08-07 12:55:05 -0700

    Day 65

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -6,43 +6,83 @@
 //
 
 import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
     
-    @State private var blurAmount = 0.0
+    @State private var image: Image?
+    @State private var filterIntensity = 0.5
+    @State private var showingImagePicker = false
+    @State private var inputImage: UIImage?
+    @State private var currentFilter = CIFilter.sepiaTone()
+    let context = CIContext()
     
-    @State private var showingConfirmation = false
-    
-    @State private var backgroundColor = Color.white
     
     var body: some View {
-//        VStack {
-//            Text("Hello world")
-//                .blur(radius: blurAmount)
-//            Slider(value: $blurAmount, in: 0...20)
-//                .onChange(of: blurAmount) {
-//                    newValue in
-//                    print("New blur amount \(newValue)")
-//                }
-//            Button("Random Blur") {
-//                blurAmount = Double.random(in: 0...20)
-//            }
-//        }
-        Text("Hello, World")
-            .frame(width: 300, height: 300)
-            .background(backgroundColor)
-            .onTapGesture {
-                showingConfirmation = true
-            }
-            .confirmationDialog("Change background", isPresented: $showingConfirmation) {
-                Button("Red") { backgroundColor = .red }
-                Button("Green") { backgroundColor = .green }
-                Button("Blue") { backgroundColor = .blue }
-                Button("Cancel", role: .cancel) {}
-            } message: {
-                Text("Select a new color")
-            }
+        NavigationView {
+            VStack {
+                ZStack {
+                    Rectangle()
+                        .fill(.secondary)
+                    Text("Tap to select a picture")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                    
+                    image?
+                        .resizable()
+                        .scaledToFit()
+                        
+                }.onTapGesture {
+                    showingImagePicker = true
+                }
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                        .onChange(of: filterIntensity) { _ in applyProcessing()
+                        }
+                }.padding(.vertical)
+                HStack {
+                    Button("Change Filter") {
+                        
+                    }
+                    Spacer()
+                    Button("Save", action: save)
+                }
+            }.padding([.horizontal, .bottom])
+                .navigationTitle("InstaFilter")
+                .onChange(of: inputImage, perform: { _ in loadImage()
+                })
+                
+                .sheet(isPresented: $showingImagePicker) {
+                    ImagePicker(image: $inputImage)
+                }
+        }
     }
+    
+    func loadImage() {
+        guard let inputImage = inputImage else {
+            return
+        }
+        
+        let beginImage = CIImage(image: inputImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
+    }
+    
+    func applyProcessing() {
+        currentFilter.intensity = Float(filterIntensity)
+        guard let outputImage = currentFilter.outputImage else { return }
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgimg)
+            image = Image(uiImage: uiImage)
+        }
+    }
+    
+    func save() {
+        
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
