fe708828beafa560e052a767eb591f0d7b089af0
phptuts <noah@kittyhawk.io>
2022-10-21 20:29:25 -0700

    Day 97

---
+++ SnowSeeker/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Noah Glaser on 10/21/22.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
